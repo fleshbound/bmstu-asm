e296a5ac94e1ed2405b9c8d58956b41e9278e3a7
Valeria Avdeykina <avdeykina@icloud.com>
2023-04-07 16:53:39 +0300

    int 1Ch was added

--- lab_06/main.asm
+++ lab_06/main.asm
@@ -10,13 +10,12 @@ CODE SEGMENT
 
 main:
     jmp initialize
-    old_int8h   dd ?
-    flag_inst   dw INSTALLED
+    old_int8h   dd  ?
+    flag_inst   dw  INSTALLED
+    curr_sec    db  0
+    speed       db  01fh
     
 int8h_handler proc far
-    pusha
-    push es
-    push ds
     pushf
     call cs:old_int8h
     
@@ -26,34 +25,29 @@ int8h_handler proc far
     cmp dh, curr_sec
     mov curr_sec, dh
     je quit
-    
+
     mov al, 0f3h
     out 60h, al
     mov al, speed
     out 60h, al
     
     dec speed
-    cmp speed, MIN_SPEED
-    je reset
+    test speed, MIN_SPEED
+    jz reset
     jmp quit
-    
+
 reset:
     mov speed, MIN_SPEED
 
 quit:
-    popf
-    pop ds
-    pop es
-    popa
-    
     iret
 int8h_handler endp
 
 initialize proc near
     ; get old handler
-    mov ax, 3508h
+    mov ax, 351Ch
     int 21h
-    
+
     cmp es:flag_inst, INSTALLED
     je uninstall
     
@@ -61,7 +55,7 @@ initialize proc near
     mov word ptr old_int8h+2, es    ; segment
     
     ; set new handler
-    mov ax, 2508h
+    mov ax, 251Ch
     mov dx, offset int8h_handler
     int 21h
     
@@ -73,44 +67,30 @@ initialize proc near
     int 27h
 
 uninstall:
-    pusha
-    push es
-    push ds
-    pushf
-    
     mov dx, word ptr es:old_int8h
     mov ds, word ptr es:old_int8h+2
-    mov ax, 2508h
+    
+    mov ax, 251Ch
     int 21h
     
-    popf
-    pop ds
-    pop es
-    popa
-    
     mov al, 0f3h
-    out 60, al
+    out 60h, al
     mov al, 0
     out 60h, al
     
+    ; fix: mov dx, offset uninst_msg
+
     mov ah, 49h
     int 21h
     
-    mov dx, offset uninst_msg
-    mov ah, 9h
-    int 21h
-    
     mov ax, 4c00h
-    int 21h
-
-init_msg    db  -'int9 installed$'
-uninst_msg  db  'int9 uninstalled$' 
-curr_sec    db  0
-speed       db  01fh
-INSTALLED   equ 0DEADh
-MIN_SPEED   equ 01fh
- 
+    int 21h 
 initialize endp
+
+MIN_SPEED   equ 01fh
+INSTALLED   equ 0DEADh
+init_msg    db  'int9 installed$'
+uninst_msg  db  'int9 uninstalled$'
 
 CODE ends
     end main
